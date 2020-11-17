6673c314c6b842b3887ac308394580ce82267fbb
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-11-17 14:30:49 +0100

    Added spring web dependency and created rest endpoint.

---
+++ src/main/java/pkkwu/Calendar/api/TestController.java
@@ -0,0 +1,12 @@
+package pkkwu.Calendar.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/test")
+	public String testController() {
+		return "Test string static";
+	}
+}
