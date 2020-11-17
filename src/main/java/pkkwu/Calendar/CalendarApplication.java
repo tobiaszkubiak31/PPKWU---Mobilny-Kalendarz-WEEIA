e696eace7ddc2ced0c0103e4621651decd88e8be
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-11-17 14:26:54 +0100

    Initialize spring boot project structure with spring initializr tool.

---
+++ src/main/java/pkkwu/Calendar/CalendarApplication.java
@@ -0,0 +1,13 @@
+package pkkwu.Calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarApplication.class, args);
+	}
+
+}
