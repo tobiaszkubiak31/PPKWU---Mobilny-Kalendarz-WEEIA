f93355ff7e271337985ace6141ae3d8a364b1f5f
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-11-17 14:38:16 +0100

    Customized created endpoint to forward year and month in url parameters.

--- src/main/java/pkkwu/Calendar/api/TestController.java
+++ src/main/java/pkkwu/Calendar/api/TestController.java
@@ -1,12 +1,15 @@
 package pkkwu.Calendar.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-	@GetMapping("/test")
-	public String testController() {
-		return "Test string static";
+
+	@GetMapping("/ics")
+	public String testController(@RequestParam(value = "month") String month,@RequestParam(value = "year") String year) {
+		return "Typed month: " + month + " year: " + year;
 	}
+
 }
