8a3127db843c8cc2230889450fcec2b578c94eb3
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-11-17 15:03:50 +0100

    Parsed url html dependent on url parameters(by month or year).

--- src/main/java/pkkwu/Calendar/api/IcsCalendarController.java
+++ src/main/java/pkkwu/Calendar/api/IcsCalendarController.java
@@ -10,26 +10,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IcsCalendarController {
 
-	public static final String URL_TO_CALENDAR = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&fbclid=IwAR0B-hAZKwveCi8CkjZ9SOUWxGLtgbL57bIE3DFxlmoVFnWx8PzTfmNRbtM";
-
+	public static final String HOSTURL = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?/kalendarz.php?";
 
 	@GetMapping("/ics")
 	public String testController(@RequestParam(value = "month") String month,
 		@RequestParam(value = "year") String year) {
-
-		return parseHtmlInGsoup();
+		String UrlToCheck = buildUrlByMonthAndDate(month,year);
+		return parseHtmlInGsoup(UrlToCheck);
 	}
 
-	String parseHtmlInGsoup(){
+	String parseHtmlInGsoup(String urlToCheck){
 		Document doc = null;
 		try {
-			doc = Jsoup.connect(URL_TO_CALENDAR).get();
+			doc = Jsoup.connect(urlToCheck).get();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return e.getMessage();
 		}
 		doc.select("p").forEach(System.out::println);
 		return doc.toString();
+	}
+
+	private String buildUrlByMonthAndDate(String month, String year) {
+		return HOSTURL + "rok=" + year + "&miesiac=" + month;
 	}
 
 }
